8e8fa901e4b39abbb2f152648d788550efe2c712
defaultuser <default@example.com>
2026-07-30 16:41:31 +0800

    X

--- index.cpp
+++ index.cpp
@@ -3,46 +3,74 @@
 #include "index.h"
 using namespace std;
 
-struct Forward{
-    Activation* d_i;
-    Activation* d_i_Minus_1;
+struct Computation
+{
+    ComputationIO<II>** input;
+    ComputationIO<OO>** output;
 };
 
-struct Backward{
-    Activation* partial_L_over_partial_d_i;
-    Activation* partial_L_over_partial_d_i_Minus_1;
+template <typename T>
+class ComputationIO
+{
+public:
+T c;
+MemoryBlock *mb;
+    ComputationIO(MemoryBlock *mb, T c)//compute_IO_type compute_IO_t,)
+    {
+this->c = c;
+this->mb = mb;
+    };
+    get_io()
+}
+
+class Forward
+{
+public:
+    Computation compute;
+    Forward()
+    {
+compute.input = ComputationIO();
+compute.output = nullptr;
+    };
 };
 
-class Device{
-    public:
-        string name;
+class Backward
+{
+public:
+    Computation compute;
+    Backward()
+    {
+
+    };
 };
 
-class Partition{
-    public:
-        string name;
+class Device
+{
+public:
+    string name;
 };
 
-class Layer{
-    public:
-        string name;
+class Partition
+{
+public:
+    string name;
 };
 
-class weight{
-    public:
-        string name;
-        int MemSize;
-        Forward forward;
+class Layer
+{
+public:
+    string name;
 };
 
-class Activation{
-    public:
-        string name;
-        int MemSize;
+class MemoryBlock
+{
+public:
+    string name;
+    int MemSize;
+    memory_description memory_type;
 };
 
-
-
-int main() {
+int main()
+{
     cout << "Hello, World!" << endl;
 }
--- index.h
+++ index.h
@@ -1,3 +1,20 @@
 
-class Activation;
-class weight;
+enum class memory_description {weight, activation};
+class MemoryBlock;
+class ComputationIO;
+
+enum class fwd_in {d_i_minus_1, weight};
+enum class fwd_out {d_i};
+
+enum class bwd_in {partial_L_over_partial_d_i, d_i_minus_1, weight};
+enum class bwd_out {partial_L_over_partial_d_i_minus_1, partial_L_over_partial_weight};
+enum class compute_IO_type {input, output};
+
+struct II{
+    fwd_in fwd_in_type;
+    bwd_in bwd_in_type;
+};
+struct OO{
+    fwd_out fwd_out_type;
+    bwd_out bwd_out_type;
+};
