371f8e0f867cf1600ff9b4a91b87593dcd1bd077
defaultuser <default@example.com>
2026-07-30 19:03:19 +0800

    .

--- index.cpp
+++ index.cpp
@@ -1,47 +1,86 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <type_traits>
+
 #include "index.h"
 using namespace std;
 
-struct Computation
-{
-    ComputationIO<II>** input;
-    ComputationIO<OO>** output;
-};
-
-template <typename T>
-class ComputationIO
+class ComputationIoSocket
 {
 public:
-T c;
-MemoryBlock *mb;
-    ComputationIO(MemoryBlock *mb, T c)//compute_IO_type compute_IO_t,)
-    {
-this->c = c;
-this->mb = mb;
-    };
-    get_io()
+    static ComputationIoSocket d_i;
+    static ComputationIoSocket partial_L_over_partial_d_i;
+    static ComputationIoSocket d_i_minus_1;
+    static ComputationIoSocket weight;
+    static ComputationIoSocket partial_L_over_partial_d_i_minus_1;
+    static ComputationIoSocket partial_L_over_partial_weight;
+    static unordered_map<Computation_description,> get_by_Compute_IO_type(){
+
+    }
+};
+
+class SocketSelector
+{
+    public:
+  
+    //enum class Fwd_in {
+  static SocketSelector  d_i_minus_1, weight 
+,
+    //};
+//enum class Fwd_out {
+d_i
+//};
+,
+//enum class Bwd_in {
+partial_L_over_partial_d_i//, d_i_minus_1, weight
+//};
+,
+//enum class Bwd_out {
+partial_L_over_partial_d_i_minus_1, partial_L_over_partial_weight
+;
+//};
+  SocketSelector(Compute_IO_type compute_IO_type,Computation_description  computation_description){
+//nest if else
+if (computation_description==Computation_description::forward){
+    if (compute_IO_type==Compute_IO_type::input){
+        //return Fwd_in::d_i_minus_1;
+    }else if (compute_IO_type==Compute_IO_type::output){
+        //return Fwd_out::d_i;
+    }else{
+        throw invalid_argument(STRING_FUCKED);
+    }
+}else if (computation_description==Computation_description::backward){
+    if (compute_IO_type==Compute_IO_type::input){
+        //return Bwd_in::partial_L_over_partial_d_i;
+    }else if (compute_IO_type==Compute_IO_type::output){
+        //return Bwd_out::partial_L_over_partial_d_i_minus_1;
+    }else{
+        throw invalid_argument(STRING_FUCKED);
+    }
+}else{
+    throw invalid_argument(STRING_FUCKED);
 }
-
-class Forward
-{
-public:
-    Computation compute;
-    Forward()
-    {
-compute.input = ComputationIO();
-compute.output = nullptr;
-    };
+    
 };
 
-class Backward
+class Computation
 {
 public:
-    Computation compute;
-    Backward()
-    {
-
-    };
+Computation_description computation_description;
+unordered_map<Compute_IO_type, ComputationIoSocket[]> sockets;
+Computation(Computation_description computation_description){
+int socket_count = -1;
+        switch (computation_description)
+        {
+            forward:
+            socket_count= static_cast<int>(Fwd_in::Count)
+            backward:
+            default:
+                throw invalid_argument(STRING_FUCKED);
+        }
+}
+    
 };
 
 class Device
--- index.h
+++ index.h
@@ -1,20 +1,12 @@
-
+#define STRING_FUCKED "u r fucked"
 enum class memory_description {weight, activation};
 class MemoryBlock;
 class ComputationIO;
 
-enum class fwd_in {d_i_minus_1, weight};
-enum class fwd_out {d_i};
+enum class Fwd_in {d_i_minus_1, weight};
+enum class Fwd_out {d_i};
 
-enum class bwd_in {partial_L_over_partial_d_i, d_i_minus_1, weight};
-enum class bwd_out {partial_L_over_partial_d_i_minus_1, partial_L_over_partial_weight};
-enum class compute_IO_type {input, output};
-
-struct II{
-    fwd_in fwd_in_type;
-    bwd_in bwd_in_type;
-};
-struct OO{
-    fwd_out fwd_out_type;
-    bwd_out bwd_out_type;
-};
+enum class Bwd_in {partial_L_over_partial_d_i, d_i_minus_1, weight};
+enum class Bwd_out {partial_L_over_partial_d_i_minus_1, partial_L_over_partial_weight};
+enum class Compute_IO_type {input, output};
+enum class Computation_description {forward, backward};
