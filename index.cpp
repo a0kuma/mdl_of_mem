ff60f612f52241633aa5d321bac493d40f542b06
defaultuser <default@example.com>
2026-07-31 00:43:49 +0800

    ~

--- index.cpp
+++ index.cpp
@@ -2,9 +2,11 @@
 #include <string>
 #include <stdexcept>
 #include <unordered_map>
+#include <map>
 #include <type_traits>
 #include <vector>
 #include <cassert>
+#include <set>
 
 #include "index.h"
 using namespace std;
@@ -51,7 +53,7 @@ class ComputationIoSockets
 {
 public:
     Compute_IO_type io_type;
-    unordered_map<Uio, ComputationIoSocket*> sockets;
+    unordered_map<Uio, ComputationIoSocket *> sockets;
     ComputationIoSockets(Compute_IO_type compute_IO_type,
                          Computation_description computation_description)
     {
@@ -60,14 +62,14 @@ public:
         {
             if (compute_IO_type == Compute_IO_type::input)
             {
-                ComputationIoSocket*  socket_ptr_d_i_minus_1 = new ComputationIoSocket(Uio::d_i_minus_1, compute_IO_type, computation_description);
+                ComputationIoSocket *socket_ptr_d_i_minus_1 = new ComputationIoSocket(Uio::d_i_minus_1, compute_IO_type, computation_description);
                 sockets.emplace(Uio::d_i_minus_1, socket_ptr_d_i_minus_1);
-                ComputationIoSocket*  socket_ptr_weight = new ComputationIoSocket(Uio::weight, compute_IO_type, computation_description);
+                ComputationIoSocket *socket_ptr_weight = new ComputationIoSocket(Uio::weight, compute_IO_type, computation_description);
                 sockets.emplace(Uio::weight, socket_ptr_weight);
             }
             else if (compute_IO_type == Compute_IO_type::output)
             {
-                ComputationIoSocket*  socket_ptr_d_i = new ComputationIoSocket(Uio::d_i, compute_IO_type, computation_description);
+                ComputationIoSocket *socket_ptr_d_i = new ComputationIoSocket(Uio::d_i, compute_IO_type, computation_description);
                 sockets.emplace(Uio::d_i, socket_ptr_d_i);
             }
             else
@@ -79,18 +81,18 @@ public:
         {
             if (compute_IO_type == Compute_IO_type::input)
             {
-                ComputationIoSocket*  socket_ptr_partial_L_over_partial_d_i = new ComputationIoSocket(Uio::partial_L_over_partial_d_i, compute_IO_type, computation_description);
+                ComputationIoSocket *socket_ptr_partial_L_over_partial_d_i = new ComputationIoSocket(Uio::partial_L_over_partial_d_i, compute_IO_type, computation_description);
                 sockets.emplace(Uio::partial_L_over_partial_d_i, socket_ptr_partial_L_over_partial_d_i);
-                ComputationIoSocket*  socket_ptr_d_i_minus_1 = new ComputationIoSocket(Uio::d_i_minus_1, compute_IO_type, computation_description);
+                ComputationIoSocket *socket_ptr_d_i_minus_1 = new ComputationIoSocket(Uio::d_i_minus_1, compute_IO_type, computation_description);
                 sockets.emplace(Uio::d_i_minus_1, socket_ptr_d_i_minus_1);
-                ComputationIoSocket*  socket_ptr_weight = new ComputationIoSocket(Uio::weight, compute_IO_type, computation_description);
+                ComputationIoSocket *socket_ptr_weight = new ComputationIoSocket(Uio::weight, compute_IO_type, computation_description);
                 sockets.emplace(Uio::weight, socket_ptr_weight);
             }
             else if (compute_IO_type == Compute_IO_type::output)
             {
-                ComputationIoSocket*  socket_ptr_partial_L_over_partial_d_i_minus_1 = new ComputationIoSocket(Uio::partial_L_over_partial_d_i_minus_1, compute_IO_type, computation_description);
+                ComputationIoSocket *socket_ptr_partial_L_over_partial_d_i_minus_1 = new ComputationIoSocket(Uio::partial_L_over_partial_d_i_minus_1, compute_IO_type, computation_description);
                 sockets.emplace(Uio::partial_L_over_partial_d_i_minus_1, socket_ptr_partial_L_over_partial_d_i_minus_1);
-                ComputationIoSocket*  socket_ptr_partial_L_over_partial_weight = new ComputationIoSocket(Uio::partial_L_over_partial_weight, compute_IO_type, computation_description);
+                ComputationIoSocket *socket_ptr_partial_L_over_partial_weight = new ComputationIoSocket(Uio::partial_L_over_partial_weight, compute_IO_type, computation_description);
                 sockets.emplace(Uio::partial_L_over_partial_weight, socket_ptr_partial_L_over_partial_weight);
             }
             else
@@ -133,15 +135,27 @@ class Device
 {
 public:
     int idx;
-    vector<Layer*> layers;
+    vector<Layer *> layers;
     vector<Partition> partitions;
+    Device(int idx)
+    {
+        this->idx = idx;
+    }
 };
 
 class Partition
 {
 public:
     int idx;
-    vector<Layer*> layers;
+    vector<Layer *> layers;
+    Partition(int idx)
+    {
+        this->idx = idx;
+    }
+    void add_layer(Layer *layer)
+    {
+        layers.push_back(layer);
+    }
 };
 
 class Layer
@@ -158,8 +172,8 @@ public:
 
 int main()
 {
-    vector<Layer*> layers;
-    vector<MemoryBlock*> memory_blocks;
+    vector<Layer *> layers;
+    vector<MemoryBlock *> memory_blocks;
     int arrayW_int[] = {56, 16, 24, 60, 100, 40, 56, 196, 84, 24, 16, 40};
     int arrayD_int[] = {28, 8, 8, 12, 20, 20, 8, 28, 28, 12, 8, 8, 20};
     assert(sizeof(arrayW_int) / sizeof(arrayW_int[0]) == (sizeof(arrayD_int) / sizeof(arrayD_int[0])) - 1);
@@ -167,25 +181,49 @@ int main()
     for (size_t i = 0; i < sizeof(arrayW_int) / sizeof(arrayW_int[0]); i++)
     {
         layers.push_back(new Layer(i + 1));
-        Layer* current_layer = layers[i];
-        
-        MemoryBlock* the_w = new MemoryBlock(i + 1, arrayW_int[i], memory_description::weight);
-		current_layer->forward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::weight)->assign_memory_block(the_w);
-		current_layer->backward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::weight)->assign_memory_block(the_w);
-		current_layer->backward_computation.io_sockets.at(Compute_IO_type::output).sockets.at(Uio::partial_L_over_partial_weight)->assign_memory_block(the_w);
-		memory_blocks.push_back(the_w);
+        Layer *current_layer = layers[i];
 
-        MemoryBlock* d_i_minus_1 = new MemoryBlock(i, arrayD_int[i], memory_description::activation);
+        MemoryBlock *the_w = new MemoryBlock(i + 1, arrayW_int[i], memory_description::weight);
+        current_layer->forward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::weight)->assign_memory_block(the_w);
+        current_layer->backward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::weight)->assign_memory_block(the_w);
+        current_layer->backward_computation.io_sockets.at(Compute_IO_type::output).sockets.at(Uio::partial_L_over_partial_weight)->assign_memory_block(the_w);
+        memory_blocks.push_back(the_w);
+
+        MemoryBlock *d_i_minus_1 = new MemoryBlock(i, arrayD_int[i], memory_description::activation);
         memory_blocks.push_back(d_i_minus_1);
         current_layer->forward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::d_i_minus_1)->assign_memory_block(d_i_minus_1);
         current_layer->backward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::d_i_minus_1)->assign_memory_block(d_i_minus_1);
         current_layer->backward_computation.io_sockets.at(Compute_IO_type::output).sockets.at(Uio::partial_L_over_partial_d_i_minus_1)->assign_memory_block(d_i_minus_1);
 
-        MemoryBlock* d_i = new MemoryBlock(i + 1, arrayD_int[i + 1], memory_description::activation);
+        MemoryBlock *d_i = new MemoryBlock(i + 1, arrayD_int[i + 1], memory_description::activation);
         memory_blocks.push_back(d_i);
         current_layer->forward_computation.io_sockets.at(Compute_IO_type::output).sockets.at(Uio::d_i)->assign_memory_block(d_i);
         current_layer->backward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::partial_L_over_partial_d_i)->assign_memory_block(d_i);
-
-
+    }
+    int balance[] = {2, 2, 2, 2, 2, 2};
+    int devices[] = {0, 0, 0, 1, 1, 1};
+    // devices array to set
+    set<int> device_set(devices, devices + sizeof(devices) / sizeof(devices[0]));
+    map<int, Device *> devices_array;
+    // for length of device set
+    for (const auto &device_idx : device_set)
+    {
+        devices_array[device_idx] = new Device(device_idx);
+    }
+    int layer_idx = 0;
+    for (size_t i = 0; i < sizeof(balance) / sizeof(balance[0]); i++)
+    {
+        Device *current_device = devices_array[devices[i]];
+        for (int j = 0; j < balance[i]; j++)
+        {
+            Partition *current_partition = new Partition(j);
+            current_device->partitions.push_back(*current_partition);
+            Layer *current_layer = layers[layer_idx];
+            current_device->layers.push_back(current_layer);
+            current_partition->add_layer(current_layer);
+            layer_idx++;
+        }
     }
 }
+
+// 正向完成 要檢查
