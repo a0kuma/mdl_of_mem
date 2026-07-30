5a96a491e3019e7ccf658de9472235dd0130f32d
defaultuser <default@example.com>
2026-07-30 20:24:09 +0800

    AI

--- index.cpp
+++ index.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 #include <unordered_map>
 #include <type_traits>
 #include <vector>
@@ -8,53 +9,17 @@
 #include "index.h"
 using namespace std;
 
-class ComputationIoSockets
+class MemoryBlock
 {
 public:
-    Compute_IO_type io_type;
-    unordered_map<Uio, ComputationIoSocket> sockets;
-    ComputationIoSockets(Compute_IO_type compute_IO_type,
-                         Computation_description computation_description)
+    int idx;
+    int MemSize;
+    memory_description memory_type;
+    MemoryBlock(int idx, int MemSize, memory_description memory_type)
     {
-        this->io_type = io_type;
-        if (computation_description == Computation_description::forward)
-        {
-            if (compute_IO_type == Compute_IO_type::input)
-            {
-                sockets[Uio::d_i_minus_1] = ComputationIoSocket(Uio::d_i_minus_1, compute_IO_type, computation_description);
-                sockets[Uio::weight] = ComputationIoSocket(Uio::weight, compute_IO_type, computation_description);
-            }
-            else if (compute_IO_type == Compute_IO_type::output)
-            {
-                sockets[Uio::d_i] = ComputationIoSocket(Uio::d_i, compute_IO_type, computation_description);
-            }
-            else
-            {
-                throw invalid_argument(STRING_FUCKED);
-            }
-        }
-        else if (computation_description == Computation_description::backward)
-        {
-            if (compute_IO_type == Compute_IO_type::input)
-            {
-                sockets[Uio::partial_L_over_partial_d_i] = ComputationIoSocket(Uio::partial_L_over_partial_d_i, compute_IO_type, computation_description);
-                sockets[Uio::d_i_minus_1] = ComputationIoSocket(Uio::d_i_minus_1, compute_IO_type, computation_description);
-                sockets[Uio::weight] = ComputationIoSocket(Uio::weight, compute_IO_type, computation_description);
-            }
-            else if (compute_IO_type == Compute_IO_type::output)
-            {
-                sockets[Uio::partial_L_over_partial_d_i_minus_1] = ComputationIoSocket(Uio::partial_L_over_partial_d_i_minus_1, compute_IO_type, computation_description);
-                sockets[Uio::partial_L_over_partial_weight] = ComputationIoSocket(Uio::partial_L_over_partial_weight, compute_IO_type, computation_description);
-            }
-            else
-            {
-                throw invalid_argument(STRING_FUCKED);
-            }
-        }
-        else
-        {
-            throw invalid_argument(STRING_FUCKED);
-        }
+        this->idx = idx;
+        this->MemSize = MemSize;
+        this->memory_type = memory_type;
     }
 };
 
@@ -62,10 +27,11 @@ class ComputationIoSocket
 {
 public:
     Uio uio;
-    MemoryBlock *memory_block;
+    MemoryBlock *memory_block = nullptr;
     ComputationIoSocket(Uio uio, Compute_IO_type compute_IO_type,
                         Computation_description computation_description)
     {
+        this->uio = uio;
     }
     void assign_memory_block(MemoryBlock *memory_block)
     {
@@ -81,6 +47,56 @@ public:
     }
 };
 
+class ComputationIoSockets
+{
+public:
+    Compute_IO_type io_type;
+    unordered_map<Uio, ComputationIoSocket> sockets;
+    ComputationIoSockets(Compute_IO_type compute_IO_type,
+                         Computation_description computation_description)
+    {
+        this->io_type = compute_IO_type;
+        if (computation_description == Computation_description::forward)
+        {
+            if (compute_IO_type == Compute_IO_type::input)
+            {
+                sockets.emplace(Uio::d_i_minus_1, ComputationIoSocket(Uio::d_i_minus_1, compute_IO_type, computation_description));
+                sockets.emplace(Uio::weight, ComputationIoSocket(Uio::weight, compute_IO_type, computation_description));
+            }
+            else if (compute_IO_type == Compute_IO_type::output)
+            {
+                sockets.emplace(Uio::d_i, ComputationIoSocket(Uio::d_i, compute_IO_type, computation_description));
+            }
+            else
+            {
+                throw invalid_argument(STRING_FUCKED);
+            }
+        }
+        else if (computation_description == Computation_description::backward)
+        {
+            if (compute_IO_type == Compute_IO_type::input)
+            {
+                sockets.emplace(Uio::partial_L_over_partial_d_i, ComputationIoSocket(Uio::partial_L_over_partial_d_i, compute_IO_type, computation_description));
+                sockets.emplace(Uio::d_i_minus_1, ComputationIoSocket(Uio::d_i_minus_1, compute_IO_type, computation_description));
+                sockets.emplace(Uio::weight, ComputationIoSocket(Uio::weight, compute_IO_type, computation_description));
+            }
+            else if (compute_IO_type == Compute_IO_type::output)
+            {
+                sockets.emplace(Uio::partial_L_over_partial_d_i_minus_1, ComputationIoSocket(Uio::partial_L_over_partial_d_i_minus_1, compute_IO_type, computation_description));
+                sockets.emplace(Uio::partial_L_over_partial_weight, ComputationIoSocket(Uio::partial_L_over_partial_weight, compute_IO_type, computation_description));
+            }
+            else
+            {
+                throw invalid_argument(STRING_FUCKED);
+            }
+        }
+        else
+        {
+            throw invalid_argument(STRING_FUCKED);
+        }
+    }
+};
+
 class Computation
 {
 public:
@@ -90,13 +106,13 @@ public:
     {
         switch (computation_description)
         {
-        forward:
-            io_sockets[Compute_IO_type::input] = ComputationIoSockets(Compute_IO_type::input, computation_description);
-            io_sockets[Compute_IO_type::output] = ComputationIoSockets(Compute_IO_type::output, computation_description);
+        case Computation_description::forward:
+            io_sockets.emplace(Compute_IO_type::input, ComputationIoSockets(Compute_IO_type::input, computation_description));
+            io_sockets.emplace(Compute_IO_type::output, ComputationIoSockets(Compute_IO_type::output, computation_description));
             break;
-        backward:
-            io_sockets[Compute_IO_type::input] = ComputationIoSockets(Compute_IO_type::input, computation_description);
-            io_sockets[Compute_IO_type::output] = ComputationIoSockets(Compute_IO_type::output, computation_description);
+        case Computation_description::backward:
+            io_sockets.emplace(Compute_IO_type::input, ComputationIoSockets(Compute_IO_type::input, computation_description));
+            io_sockets.emplace(Compute_IO_type::output, ComputationIoSockets(Compute_IO_type::output, computation_description));
             break;
         default:
             throw invalid_argument(STRING_FUCKED);
@@ -128,20 +144,6 @@ public:
     }
 };
 
-class MemoryBlock
-{
-public:
-    int idx;
-    int MemSize;
-    memory_description memory_type;
-    MemoryBlock(int idx, int MemSize, memory_description memory_type)
-    {
-        this->idx = idx;
-        this->MemSize = MemSize;
-        this->memory_type = memory_type;
-    }
-};
-
 int main()
 {
     vector<Layer> layers;
@@ -154,15 +156,15 @@ int main()
     {
         layers.push_back(Layer(i + 1));
         memory_blocks.push_back(MemoryBlock(i + 1, arrayW_int[i], memory_description::weight));
-        layers.back().forward_computation.io_sockets[Compute_IO_type::input].sockets[Uio::weight].assign_memory_block(&(memory_blocks.back()));
-        layers.back().backward_computation.io_sockets[Compute_IO_type::input].sockets[Uio::weight].assign_memory_block(&(memory_blocks.back()));
-        layers.back().backward_computation.io_sockets[Compute_IO_type::output].sockets[Uio::partial_L_over_partial_weight].assign_memory_block(&(memory_blocks.back()));
+        layers.back().forward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::weight).assign_memory_block(&(memory_blocks.back()));
+        layers.back().backward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::weight).assign_memory_block(&(memory_blocks.back()));
+        layers.back().backward_computation.io_sockets.at(Compute_IO_type::output).sockets.at(Uio::partial_L_over_partial_weight).assign_memory_block(&(memory_blocks.back()));
         memory_blocks.push_back(MemoryBlock(i, arrayD_int[i], memory_description::activation));
-        layers.back().forward_computation.io_sockets[Compute_IO_type::input].sockets[Uio::d_i_minus_1].assign_memory_block(&(memory_blocks.back()));
-        layers.back().backward_computation.io_sockets[Compute_IO_type::input].sockets[Uio::d_i_minus_1].assign_memory_block(&(memory_blocks.back()));
-        layers.back().backward_computation.io_sockets[Compute_IO_type::output].sockets[Uio::partial_L_over_partial_weight].assign_memory_block(&(memory_blocks.back()));
+        layers.back().forward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::d_i_minus_1).assign_memory_block(&(memory_blocks.back()));
+        layers.back().backward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::d_i_minus_1).assign_memory_block(&(memory_blocks.back()));
+        layers.back().backward_computation.io_sockets.at(Compute_IO_type::output).sockets.at(Uio::partial_L_over_partial_d_i_minus_1).assign_memory_block(&(memory_blocks.back()));
         memory_blocks.push_back(MemoryBlock(i + 1, arrayD_int[i + 1], memory_description::activation));
-        layers.back().forward_computation.io_sockets[Compute_IO_type::output].sockets[Uio::d_i].assign_memory_block(&(memory_blocks.back()));
-        layers.back().backward_computation.io_sockets[Compute_IO_type::input].sockets[Uio::partial_L_over_partial_d_i].assign_memory_block(&(memory_blocks.back()));
+        layers.back().forward_computation.io_sockets.at(Compute_IO_type::output).sockets.at(Uio::d_i).assign_memory_block(&(memory_blocks.back()));
+        layers.back().backward_computation.io_sockets.at(Compute_IO_type::input).sockets.at(Uio::partial_L_over_partial_d_i).assign_memory_block(&(memory_blocks.back()));
     }
 }
