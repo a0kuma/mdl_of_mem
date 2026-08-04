0a9a2556d5b56536084bf92e0c96ff8c64b7eb92
defaultuser <default@example.com>
2026-08-04 16:02:15 +0800

    .

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ echo "globalThis.MemoryView = MemoryView;" >> MemoryViz.js
 echo "globalThis.snapshot_cache = snapshot_cache;" >> MemoryViz.js
 echo "globalThis.snapshot_select = snapshot_select;" >> MemoryViz.js
 echo 'globalThis.process_alloc_data = process_alloc_data;' >> MemoryViz.js
-cp ../vselect.js
+cp ../vselect.js .
 sed -i 's#</body>#<script src="./vselect.js"></script>\n</body>#' memory_viz.html
