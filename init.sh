6b24450dcdbe6b2f50d3a3cb961ca58cf647a356
defaultuser <default@example.com>
2026-08-04 15:51:53 +0800

    .

--- init.sh
+++ init.sh
@@ -6,3 +6,5 @@ sed -i 's/https\:\/\/cdn\.jsdelivr\.net\/gh\/pytorch\/pytorch@main\/torch\/utils
 echo "globalThis.MemoryView = MemoryView;" >> MemoryViz.js 
 echo "globalThis.snapshot_cache = snapshot_cache;" >> MemoryViz.js
 echo "globalThis.snapshot_select = snapshot_select;" >> MemoryViz.js
+echo 'globalThis.process_alloc_data = process_alloc_data;' >> MemoryViz.js
+sed -i 's/^elements_length: elements\.length,$/elements_length: elements.length,\n elements,/' process_alloc_data.js
